98875af15041c5d97fce2fcc2d659dc8aa210aa5
Alexandru Gheorghe <alghe.global@gmail.com>
2023-12-02 15:22:35 +0000

    [TipTime] Add custom tip capability, fix alignment of Text with Switch, add leading icons

--- AndroidBasicsCourse/TipTime/app/build.gradle.kts
+++ AndroidBasicsCourse/TipTime/app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity:1.8.1")
     implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.compose.foundation:foundation:1.0.0")
+    implementation("androidx.compose.foundation:foundation-layout:1.0.0")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
