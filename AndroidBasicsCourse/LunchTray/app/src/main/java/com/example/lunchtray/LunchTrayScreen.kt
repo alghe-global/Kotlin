d397e25dc31f8bc314ff0748debf90ce50136dc0
Alexandru Gheorghe <alghe.global@gmail.com>
2024-01-16 11:34:05 +0000

    [LunchTray] LunchTrayScreen.kt: remove unnecessary imports as well as Modifier.fillMaxHeight() for Entree menu

--- AndroidBasicsCourse/LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ AndroidBasicsCourse/LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -25,10 +25,8 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -135,8 +133,7 @@ fun LunchTrayApp(
                         navController.navigate(LunchTrayScreen.SideDish.name)
                     },
                     onSelectionChanged = { viewModel.updateEntree(it) },
-                    options = DataSource.entreeMenuItems,
-                    modifier = Modifier.fillMaxHeight()
+                    options = DataSource.entreeMenuItems
                 )
             }
 
