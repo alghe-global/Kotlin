ef9d5e2b5c64ce89e8f0dfe559a733aee29544a4
Alexandru Gheorghe <alghe.global@gmail.com>
2024-01-18 14:54:48 +0000

    [Reply] Introduce adaptive layout with dynamic navigation

--- AndroidBasicsCourse/Reply/app/build.gradle.kts
+++ AndroidBasicsCourse/Reply/app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.06.00"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
