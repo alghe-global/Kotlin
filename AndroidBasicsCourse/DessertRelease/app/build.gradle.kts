10b62bc7a145b20371e96aebfc236d0dd0724ab6
Alexandru Gheorghe <alghe.global@gmail.com>
2024-02-29 15:17:45 +0000

    [DessertRelease] Set up dependencies, implement the user preferences repository, initialize the DataStore, use the UserPreferencesRepository

--- AndroidBasicsCourse/DessertRelease/app/build.gradle.kts
+++ AndroidBasicsCourse/DessertRelease/app/build.gradle.kts
@@ -65,7 +65,6 @@ android {
 }
 
 dependencies {
-
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
@@ -75,6 +74,8 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
+    // Preferences DataStore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
